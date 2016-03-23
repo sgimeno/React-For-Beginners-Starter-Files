86cf74a4344da6d8738a341542d74ca28437af12
Sergio Gimeno <gimeno.sergio@gmail.com>
2016-03-23 16:58:19 +0100

    feat(session4): follow session 4 video

--- 01 - Introduction - Start Here/scripts/main.js
+++ 01 - Introduction - Start Here/scripts/main.js
@@ -9,8 +9,13 @@ var ReactDOM = require('react-dom');
 var StorePicker = React.createClass({
 
   render: function(){
+    var name = 'wes';
     return (
-      <p>hello</p>
+      <form className="store-selector">
+        <h2>Please Enter A Store {name}</h2>
+        <input type="text" ref="storeId" required/>
+        <input type="submit"/>
+      </form>
     )
   }
 
