b8edfd824c84837dd1b06c4e9819a2547695e963
Sergio Gimeno <gimeno.sergio@gmail.com>
2016-03-23 16:42:53 +0100

    feat(sessions1,2,3): follow videos 1,2,3

---
+++ 01 - Introduction - Start Here/scripts/main.js
@@ -0,0 +1,19 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+/*
+  StorePicker
+  return <StorePicker/> component
+*/
+
+var StorePicker = React.createClass({
+
+  render: function(){
+    return (
+      <p>hello</p>
+    )
+  }
+
+});
+
+ReactDOM.render(<StorePicker/>, document.getElementById('main'));
